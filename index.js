55f8b05fa9ada45a94ef516b19153ec60f2f2a23
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-13 15:16:11 +0600

    cors origin updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:4173"],
+    origin: ["http://localhost:5173", "http://localhost:4173","https://heritage-nahid.netlify.app"],
   })
 );
 
